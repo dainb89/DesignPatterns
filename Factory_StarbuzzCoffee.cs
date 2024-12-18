b0c549694e4eeb6c62b928cdde688ddcf639e394
baodainguyen <baodaihd89@gmail.com>
2024-12-18 20:30:16 +0700

    learn factory pattern

--- Factory_StarbuzzCoffee.cs
+++ Factory_StarbuzzCoffee.cs
@@ -7,11 +7,21 @@ public class Program
     {
         CoffeFactory cFactory = new CoffeFactory();
 		CoffeStore cStore = new CoffeStore(cFactory);
-        Console.WriteLine(cStore.OrderCoffe("1 Espresso with whip and steamed Milk"));
-        Console.WriteLine(cStore.OrderCoffe("decaf with 2 Steamed milk"));
-		Console.WriteLine(cStore.OrderCoffe("venti Decaf with 2 Steamed milk"));
-        Console.WriteLine(cStore.OrderCoffe("dark Roast with 2 mocha and 1 soy"));
-        Console.WriteLine(cStore.OrderCoffe("1 tall Dark Roast with 2 mocha and 1 soy"));
+		string tOrd = "1 Espresso with whip and steamed Milk";
+		Console.WriteLine("Order =>: " + tOrd);
+        Console.WriteLine(cStore.OrderCoffe(tOrd));
+		tOrd = "decaf with 2 Steamed milk";
+		Console.WriteLine("Order =>: " + tOrd);
+        Console.WriteLine(cStore.OrderCoffe(tOrd));
+		tOrd = "venti Decaf with 2 Steamed milk";
+		Console.WriteLine("Order =>: " + tOrd);
+		Console.WriteLine(cStore.OrderCoffe(tOrd));
+		tOrd = "dark Roast with 2 mocha and 1 soy";
+		Console.WriteLine("Order =>: " + tOrd);
+        Console.WriteLine(cStore.OrderCoffe(tOrd));
+		tOrd = "1 tall Dark Roast with 2 mocha and 1 soy";
+		Console.WriteLine("Order =>: " + tOrd);
+        Console.WriteLine(cStore.OrderCoffe(tOrd));
     }
 }
 public class CoffeStore
@@ -21,8 +31,7 @@ public class CoffeStore
     public string OrderCoffe(string orderTxt)
     {
         string sampleOrder = "Plz order like this: House Blend with Steamed Milk, 2 Mocha, 1 soy and Whip";
-        if (String.IsNullOrEmpty(orderTxt) || String.IsNullOrWhiteSpace(orderTxt))
-        {
+        if(String.IsNullOrEmpty(orderTxt) || String.IsNullOrWhiteSpace(orderTxt)) {
             return sampleOrder;
         }
         // #region replace 2 or more space into one
@@ -34,66 +43,54 @@ public class CoffeStore
         Beverage beverage;
         if (ordTxtLow.Contains("espresso")) beverage = factory.CreateCoffe("Espresso");
         else if (ordTxtLow.Contains("dark roast")) beverage = factory.CreateCoffe("DarkRoast");
-        else if (ordTxtLow.Contains("decaf")) beverage = factory.CreateCoffe("Decaf");
-        else if (ordTxtLow.Contains("house blend")) beverage = factory.CreateCoffe("House Blend");
-        else return sampleOrder;
-        string tSize = Size.GRANDE.ToString();
-        if (ordTxtLow.Contains("venti"))
-        {
-            beverage.SetSize(Size.VENTI);
-            tSize = Size.VENTI.ToString();
-        }
-        else if (ordTxtLow.Contains("tall"))
-        {
-            beverage.SetSize(Size.TALL);
-            tSize = Size.TALL.ToString();
-        }
+		else if (ordTxtLow.Contains("decaf")) beverage = factory.CreateCoffe("Decaf");
+		else if (ordTxtLow.Contains("house blend")) beverage = factory.CreateCoffe("House Blend");
+		else return sampleOrder;
+		string tSize = "Grande";
+		if (ordTxtLow.Contains("venti")) {
+			beverage.SetSize(Size.VENTI);
+			tSize = "Venti";
+		}
+		else if (ordTxtLow.Contains("tall")) {
+			beverage.SetSize(Size.TALL);
+			tSize = "Tall";
+		}
 
         string[] arOrdTxt = ordTxtLow.Split(' ');
-
         for (int ii = 1; ii < arOrdTxt.Length; ii++)
         {
             var txt = arOrdTxt[ii];
-            var prTxt = arOrdTxt[ii - 1];
-            if (txt.Contains("mocha"))
-            {
-                beverage = CreateCondiment("Mocha", prTxt, beverage);
-            }
-            if (txt.Contains("soy"))
-            {
-                beverage = CreateCondiment("Soy", prTxt, beverage);
-            }
-            if (txt.Contains("whip"))
-            {
-                beverage = CreateCondiment("Whip", prTxt, beverage);
-            }
+			var prTxt = arOrdTxt[ii - 1];
+            if (txt.Contains("mocha")) beverage = CreateCondiment("Mocha", prTxt, beverage);
+            if (txt.Contains("soy")) beverage = CreateCondiment("Soy", prTxt, beverage);
+            if (txt.Contains("whip")) beverage = CreateCondiment("Whip", prTxt, beverage);
             if (txt.Contains("steamed") && ii < arOrdTxt.Length - 1 && arOrdTxt[ii + 1].Contains("milk"))
             {
                 beverage = CreateCondiment("Steamed Milk", prTxt, beverage);
             }
         }
-        var des = string.Join(", ", beverage.getDescription());
-        return "Ready: 1 " + tSize + " " + des + " ($" + beverage.cost() + ")";
+		var des = string.Join(", ", beverage.getDescription());
+		return "=> Ready: 1 " + tSize + " " + des + " ($" + beverage.cost() + ")";
     }
     private Beverage CreateCondiment(string name, string prTxt, Beverage b)
     {
-        Beverage beverage = b;
+		Beverage beverage = b;
         if (prTxt.Contains("with") || prTxt.Contains("and") || prTxt.Contains(","))
         {
             return factory.CreateCondiment(name, beverage);
         }
         try
-        {
-            int cc = Int32.Parse(prTxt);
-            for (int iic = 0; iic < cc; iic++)
-            {
-                beverage = factory.CreateCondiment(name, beverage);
-            }
-        }
-        catch (FormatException)
-        {
-            Console.WriteLine("Unable to parse " + prTxt);
-        }
+		{
+			int cc = Int32.Parse(prTxt);
+			for (int iic = 0; iic < cc; iic++)
+			{
+				beverage = factory.CreateCondiment(name, beverage );
+			}
+		}
+		catch (FormatException)
+		{
+			Console.WriteLine("Unable to parse " + prTxt);
+		}
         return beverage;
     }
 }
